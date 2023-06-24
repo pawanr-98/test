27dda3fd525c8c76a63a1a68f6cb73bcaad8dfb7
pawan <rohankarpawan49@gmail.com>
2023-06-24 18:23:32 +0530

    Value of a changed

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 a=10
 
-if a<20
+if a<30
     print(a,"is less than 20")
 elif a=20
     print(a,"is is equal to 20")
@@ -8,3 +8,4 @@ else
     print(a,"is greater than 20")
 
 #This python code contain some errors and need to be fixed
+
